8752c3c8867077600dfd5c2fead82bb12e925437
deskjet <simon.guendling@gmx.de>
2013-04-21 17:37:39 +0200

    nice wrapper (load,play,pause,resume,unload)

---
+++ chiptune.js
@@ -0,0 +1,204 @@
+function ChiptunePlayer(destination) {
+  this.output = destination;
+}
+
+ChiptunePlayer.prototype.fileCounter = 0;
+
+ChiptunePlayer.prototype.generateFilename = function() {
+  return "modfile" + this.fileCounter++;
+}
+
+ChiptunePlayer.prototype.load = function load(input, cb) {
+  var onFileReady = function onFileReady() {
+    this.player_ptr = this.xmp.init(this.path);
+    if (this.player_ptr == 0) {
+      try { FS.deleteFile(this.path); } catch(err) {};
+      return cb("could not initialize player");
+    };
+
+    return cb();
+  }.bind(this);
+
+  if (input instanceof File) {
+    var reader = new FileReader();
+      reader.onload = function() {
+        var filename = this.generateFilename();
+        this.path = "/" + filename;
+
+        FS.createDataFile('/', filename, new Int8Array(reader.result), true, true);
+        onFileReady();
+      }
+      reader.readAsArrayBuffer(input);
+  }
+
+  if (typeof input === "string") {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', input, true);
+    xhr.responseType = 'arraybuffer';
+
+    var filename = this.generateFilename();
+    this.path = "/" + filename;
+
+    xhr.onload = function(e) {
+      FS.createDataFile('/', filename, new Int8Array(xhr.response), true, true);
+      onFileReady();
+    };
+
+    xhr.send();
+  }
+};
+
+ChiptunePlayer.prototype.unload = function() {
+  var initialized = this.xmp.player_data[this.player_ptr] || false;
+  var paused = this.xmp.player_data[this.player_ptr].pause;
+  var playing = initialized && !paused;
+
+  // mute - can't harm in any case
+  this.pause();
+
+  if (initialized) {
+    // stop on next chunk and prevent resume
+    this.xmp.player_data[this.player_ptr].stop = true;
+  }
+
+  if (!playing) {
+    // not playing, so stop flag won't work - clean now
+    this.xmp.free_player(this.player_ptr);
+    delete this.xmp.player_data[this.player_ptr];
+  }
+};
+
+ChiptunePlayer.prototype.play = function() {
+  this.xmp.play.bind(this)(this.player_ptr, 0, true);
+};
+
+ChiptunePlayer.prototype.pause = function() {
+  this.xmp.pause.bind(this)(this.player_ptr);
+};
+
+ChiptunePlayer.prototype.resume = function() {
+  this.xmp.resume.bind(this)(this.player_ptr);
+};
+
+
+
+ChiptunePlayer.prototype.xmp = {
+  init: Module.cwrap('initialize_player', 'number', ['string']),
+  read: Module.cwrap('read_from_player', 'number', ['number']),
+  free_buffer: Module.cwrap('free_buffer', 'null', ['number']),
+  free_player: Module.cwrap('free_player', 'null', ['number']),
+  player_data: {},
+  get_audio_source: function(player_ptr) {
+    // Helper: Read one chunk from player
+    var get_buffer = function(player_ptr) {
+      // Helper: Retreive data from buffer as Float32Array
+      var get_data = function(buf_ptr) {
+        var data_ptr = Module.getValue(buf_ptr, '*');
+        var size = Module.getValue(buf_ptr + 4, 'i32');
+        if (size == 0  || data_ptr == 0) return;
+
+        var i8Array = Module.HEAP8.subarray(data_ptr, data_ptr + size);
+        return new Float32Array(i8Array.buffer, data_ptr, size / 4);
+      }
+
+      var buf_ptr = this.xmp.read(player_ptr);
+      var ret = get_data(buf_ptr);
+      this.xmp.free_buffer(buf_ptr);
+      return ret;
+    }.bind(this);
+
+    // Append a number of audio chunks
+    var raw_audio = get_buffer(player_ptr);
+    if (raw_audio === undefined) return;
+
+    for (var i = 0; i < 64; i++) {
+      var new_data = get_buffer(player_ptr);
+      if (new_data === undefined) break;
+      var old_data = raw_audio;
+      raw_audio = new Float32Array(old_data.length + new_data.length);
+      raw_audio.set(old_data, 0);
+      raw_audio.set(new_data, old_data.length);
+    }
+    delete new_data;
+    delete old_data;
+
+    // Create a JS AudioBuffer
+    var audio_buffer = context.createBuffer(1, raw_audio.length, 44100);
+    audio_buffer.getChannelData(0).set(raw_audio);
+
+    // Put it into a JS BufferSource
+    var source = context.createBufferSource();
+    source.buffer = audio_buffer;
+    source.connect(this.output);
+    return source;
+  },
+  play: function(player_ptr, last_chunk, first_run) {
+    // Fill buffer on first run
+    if (first_run) {
+      this.xmp.player_data[player_ptr] = {};
+      this.xmp.player_data[player_ptr].next_src = this.xmp.get_audio_source.bind(this, player_ptr)();
+    }
+
+    // Check for stop-flag
+    if (this.xmp.player_data[player_ptr].stop === true) {
+      this.xmp.free_player(this.player_ptr);
+      delete this.xmp.player_data[player_ptr];
+      return;
+    }
+
+    // no more data -> clean up
+    if (!this.xmp.player_data[player_ptr].next_src) {
+      this.xmp.free_player(player_ptr);
+      return;
+    }
+
+    // play!
+    if (!this.xmp.player_data[player_ptr].pause) {
+      var duration = this.xmp.player_data[player_ptr].next_src.buffer.duration;
+      var last_chunk = first_run ? context.currentTime : last_chunk + duration;
+      this.xmp.player_data[player_ptr].last_src = this.xmp.player_data[player_ptr].current_src;
+      this.xmp.player_data[player_ptr].current_src = this.xmp.player_data[player_ptr].next_src;
+
+      this.xmp.player_data[player_ptr].current_src.start(last_chunk);
+      this.xmp.player_data[player_ptr].current_src.timeScheduled = last_chunk;
+
+      // schedule next chunk
+      var timerId = setTimeout(this.xmp.play.bind(this, player_ptr, last_chunk), first_run ? (duration * 1000) / 2 : (duration * 1000));
+      this.xmp.player_data[player_ptr].timerId = timerId;
+
+      // fill buffer while playing
+      this.xmp.player_data[player_ptr].next_src = this.xmp.get_audio_source.bind(this)(player_ptr);
+    }
+  },
+  pause: function(player_ptr) {
+    // stop/unschedule everything and save current time
+    if (this.xmp.player_data[player_ptr]) {
+      this.xmp.player_data[player_ptr].last_src && this.xmp.player_data[player_ptr].last_src.stop(0);
+      this.xmp.player_data[player_ptr].current_src && this.xmp.player_data[player_ptr].current_src.stop(0);
+      this.xmp.player_data[player_ptr].next_src && this.xmp.player_data[player_ptr].next_src.stop(0);
+      this.xmp.player_data[player_ptr].pause = context.currentTime;
+      clearTimeout(this.xmp.player_data[player_ptr].timerId);
+    }
+  },
+  resume: function(player_ptr) {
+    // only resume if paused and not stopped
+    if (this.xmp.player_data[player_ptr] && this.xmp.player_data[player_ptr].pause && !this.xmp.player_data[player_ptr].stop) {
+      // calculate point in buffer at the time the player paused
+      var current_offset = context.currentTime - this.xmp.player_data[player_ptr].current_src.timeScheduled;
+
+      // play buffer from calculated point
+      if (current_offset < this.xmp.player_data[player_ptr].current_src.buffer.duration) {
+        var new_src = context.createBufferSource();
+        new_src.buffer = this.xmp.player_data[player_ptr].current_src.buffer;
+        new_src.connect(this.output);
+        new_src.start(0);
+      }
+
+      // unpause
+      this.xmp.player_data[player_ptr].pause = undefined;
+
+      // continue normally
+      this.xmp.play.bind(this)(player_ptr, 0, false);
+    }
+  }
+};
